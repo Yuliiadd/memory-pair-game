3531027d1a7f838429e542f409bf0156ad1f80b8
Yuliia <yuliiadd@gmail.com>
2022-09-10 12:45:52 +0300

    refactor: added tablet/mobile  version

--- app.js
+++ app.js
@@ -4,33 +4,46 @@ cards = document.querySelectorAll(".card"),
 score = document.querySelector(".score__value"),
 restart = document.querySelector(".btn"),
 collection = document.querySelector(".collection");
+
 let openedCardsSrc = [];
 let openedCards = [];
 let foldedPairs = 0;
-let cardNames = ['c1', 'c2', 'c3', 'd4', 'd5', 'd6', 'c1', 'c2', 'c3', 'd4', 'd5', 'd6']
+let cardNames = ['c1', 'c2', 'c3', 'd4', 'd5', 'd6', 'c1', 'c2', 'c3', 'd4', 'd5', 'd6'];
+
+
+// event listeners
 
 document.addEventListener('DOMContentLoaded', render);
 field.addEventListener("click", flipTheCard);
 restart.addEventListener("click", render);
 
+// function fliping cards & create arrays of opened cards
+
 function flipTheCard(e) {
     let frontSide = e.target;
-
-    if (frontSide.tagName === "IMG" && openedCardsSrc.length < 2) {
+    openedCards = document.querySelectorAll(".open__card");
+    if (frontSide.tagName === "IMG" && openedCards.length < 2) {
+        console.log(openedCards);
         let openedCard = e.target.parentElement;
+        openedCard.classList.add("open__card");
+        openedCards = document.querySelectorAll(".open__card");
         let backSide = e.target.previousElementSibling;
         frontSide.style.transform = "rotateY(180deg)"
         backSide.style.transform = "rotateY(360deg)"
         backSide.style.opacity = "100";
-        openedCard.classList.add("open__card");
         openedCardsSrc.push(backSide.src.slice(-6));
-        openedCards = document.querySelectorAll(".open__card");
+        console.log(openedCardsSrc);
     };
 
     if (openedCards.length == 2) {
         checkThePairs();
     };
+
+    if (frontSide.tagName === "DIV") return;
+
 };
+
+// checking opened cards
 
 function checkThePairs() {
     if (openedCardsSrc[0] === openedCardsSrc[1] && foldedPairs < 6) {
@@ -44,7 +57,7 @@ function checkThePairs() {
         for (let i=1; i<=2; i++) {
             let miniCard = document.createElement("img");
             miniCard.classList.add('miniCard');
-            miniCard.src = `img/${openedCardsSrc[0]}`;
+            miniCard.src = `img/${openedCardsSrc[1]}`;
             pair.append(miniCard);
             pair.style.marginLeft = "10px";
             pair.style.marginRight = "10px";
@@ -77,11 +90,16 @@ function checkThePairs() {
     };
 };
 
+// randomizer for cards
+
 function shuffle(arr) {
     return arr.sort(() => Math.random() - 0.5);
 };
 
+// render for cards
+
 function render() {
+    score.innerHTML = "0";
     shuffle(cardNames);
     field.innerHTML = '';
     field.classList.add('game__field');
